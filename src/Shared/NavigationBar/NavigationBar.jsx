d8109c94be8647ba13b539c74c4309eff4f52f94
Hasnathasan <hasnatoooooooo@gmail.com>
2024-02-02 15:00:09 +0600

    Top info added

--- src/Shared/NavigationBar/NavigationBar.jsx
+++ src/Shared/NavigationBar/NavigationBar.jsx
@@ -1,11 +1,25 @@
-
+import { CiLocationOn } from "react-icons/ci";
 
 const NavigationBar = () => {
-    return (
-        <div>
-            <h1>This is Navigation Menu</h1>
+  return (
+    <div>
+      {/* Top Info - Company name + sign In / sign up button */}
+      <div className="flex justify-between items-center bg-gray-900 text-gray-400 text-xs py-1">
+        <div className="flex justify-center items-center gap-1">
+          <CiLocationOn />
+          <h3>Store Location: Lincoln- 344, Illinois, Chicago, USA</h3>
         </div>
-    );
+        <div>
+            <div className="flex justify-center items-center">
+                <h3>Sign In</h3> <span>/</span> <h3>Sign Up</h3>
+            </div>
+        </div>
+      </div>
+
+
+
+    </div>
+  );
 };
 
 export default NavigationBar;
