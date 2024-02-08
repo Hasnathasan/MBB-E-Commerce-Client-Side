cf45fd4baa55aa7d991ae98ca1b45f6a07b5a419
Hasnathasan <hasnatoooooooo@gmail.com>
2024-02-08 23:33:38 +0600

    Cart.jsx file added

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,9 +1,45 @@
-
+import product1 from '../../assets/products1.png'
 
 const Cart = () => {
     return (
-        <div className="min-h-[500px] flex justify-center items-center">
-            <h2 className="text-3xl font-bold text-green-600">This is Cart</h2>
+        <div className="grid grid-cols-12">
+            <div className=' border col-span-8 border-gray-300 rounded-lg overflow-hidden'>
+            <table className="overflow-auto w-full">
+        <tr>
+          <th>PRODUCT</th>
+          <th>PRICE</th>
+          <th>QUANTITY</th>
+          <th>SUBTOTAL</th>
+        </tr>
+        <tr>
+          <td className="flex items-center gap-2">
+            <img className="w-16" src={product1} alt="" />
+            <h3 className="font-semibold">The Starry Night</h3>
+          </td>
+          <td>$549</td>
+          <td>x5</td>
+          <td>$2638</td>
+        </tr>
+        <tr>
+          <td className="flex items-center gap-2">
+            <img className="w-16" src={product1} alt="" />
+            <h3 className="font-semibold">The Starry Night</h3>
+          </td>
+          <td>$549</td>
+          <td>x5</td>
+          <td>$2638</td>
+        </tr>
+        <tr>
+          <td className="flex items-center gap-2">
+            <img className="w-16" src={product1} alt="" />
+            <h3 className="font-semibold">The Starry Night</h3>
+          </td>
+          <td>$549</td>
+          <td>x5</td>
+          <td>$2638</td>
+        </tr>
+      </table>
+            </div>
         </div>
     );
 };
