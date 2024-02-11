fcdf643bd4128b2e7641c27efbfe14d823692a8a
Hasnathasan <hasnatoooooooo@gmail.com>
2024-02-11 22:07:35 +0600

    useUser.jsx custom hook created to get user data

--- src/Hooks/useUser.jsx
+++ src/Hooks/useUser.jsx
@@ -1,19 +1,20 @@
-// import { useQuery } from "@tanstack/react-query";
-// import { useContext } from "react";
-// import { AuthContext } from "../Providers/AuthProvider";
-// import useAxiosSecure from "./useAxiosSecure";
+import { useContext } from "react";
+import useAxiosSecure from "./useAxiosSecure";
+import { AuthContext } from "../Providers/AuthProvider";
+import { useQuery } from "@tanstack/react-query";
 
-// const useUser = () => {
-//     const [axiosSecure] = useAxiosSecure()
-//     const {user} = useContext(AuthContext);
-//     const { data: userData, isLoading: isUserDataLoading, refetch } = useQuery({
-//         queryKey: [`user-${user?.email}`],
-//         queryFn: async() => {
-//             const res = await axiosSecure.get(`/${user?.email ? `eachUser/${user?.email}` : `each-user-by-number/${user?.phoneNumber}`}`)
-//             return res.data;
-//         },
-//       })
-//       return [userData, isUserDataLoading, refetch]
-// };
 
-// export default useUser;
+const useUser = () => {
+    const [axiosSecure] = useAxiosSecure()
+    const {user} = useContext(AuthContext);
+    const { data: userData, isLoading: isUserDataLoading, refetch } = useQuery({
+        queryKey: [`user-${user?.email}`],
+        queryFn: async() => {
+            const res = await axiosSecure.get(`/${user?.email}`)
+            return res.data;
+        },
+      })
+      return [userData, isUserDataLoading, refetch]
+};
+
+export default useUser;
