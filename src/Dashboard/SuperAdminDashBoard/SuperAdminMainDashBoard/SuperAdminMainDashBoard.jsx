e38811cadcc0f728660bc45c2072b7a383743c64
Hasnathasan <hasnatoooooooo@gmail.com>
2024-03-12 19:44:30 +0600

    SuperAdminMainDashBoard.jsx file formated with prettier

--- src/Dashboard/SuperAdminDashBoard/SuperAdminMainDashBoard/SuperAdminMainDashBoard.jsx
+++ src/Dashboard/SuperAdminDashBoard/SuperAdminMainDashBoard/SuperAdminMainDashBoard.jsx
@@ -1,22 +1,29 @@
 import { Button } from "@nextui-org/react";
 import { BsFillPeopleFill, BsPersonCircle } from "react-icons/bs";
-import { FaBookmark, FaHome, FaProductHunt, FaShoppingCart } from "react-icons/fa";
+import {
+  FaBookmark,
+  FaHome,
+  FaProductHunt,
+  FaShoppingCart,
+} from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
-import logo from '../../../assets/logo.png'
-import './SuperAdminMainDashBoard.css'
+import logo from "../../../assets/logo.png";
+import "./SuperAdminMainDashBoard.css";
 const SuperAdminMainDashBoard = () => {
-    const mainLinks = <>
-  <li>
-            <NavLink className="p-3 text-base" to="/">
-              <FaHome></FaHome> Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className="p-3 text-base" to="/cart">
-              <FaShoppingCart></FaShoppingCart> Cart
-            </NavLink>
-          </li>
-  </>
+  const mainLinks = (
+    <>
+      <li>
+        <NavLink className="p-3 text-base" to="/">
+          <FaHome></FaHome> Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink className="p-3 text-base" to="/cart">
+          <FaShoppingCart></FaShoppingCart> Cart
+        </NavLink>
+      </li>
+    </>
+  );
   return (
     <div className="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
@@ -30,15 +37,20 @@ const SuperAdminMainDashBoard = () => {
           Open drawer
         </label> */}
         <div className=" bg-white shadow rounded-md mx-2 flex justify-center items-center p-3 absolute top-2 lg:hidden left-0 right-0">
-        <Button
-                  className="basis-1/2 hover:!text-white"
-                  size="sm"
-                  color="primary"
-                  radius="none"
-                  variant="solid"
-                >
-                  <label htmlFor="my-drawer-2" className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center drawer-button">Open Dashboard</label>
-                </Button>
+          <Button
+            className="basis-1/2 hover:!text-white"
+            size="sm"
+            color="primary"
+            radius="none"
+            variant="solid"
+          >
+            <label
+              htmlFor="my-drawer-2"
+              className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center drawer-button"
+            >
+              Open Dashboard
+            </label>
+          </Button>
         </div>
       </div>
       <div className="drawer-side w-full">
@@ -46,13 +58,13 @@ const SuperAdminMainDashBoard = () => {
         {/* Sidebar content here */}
         <div className="w-80"></div>
         <ul className="menu flex-nowrap top-0 fixed px-8 h-screen bg-gray-200 py-10 w-80 space-y-2">
-        <img className="w-64 h-24" src={logo} alt="" />
-        <li>
+          <img className="w-64 h-24" src={logo} alt="" />
+          <li>
             <NavLink className="p-3 text-base" to="overview">
               <BsPersonCircle></BsPersonCircle> Dashboard
             </NavLink>
           </li>
-        <li>
+          <li>
             <NavLink className="p-3 text-base" to="orders">
               <BsPersonCircle></BsPersonCircle> Orders
             </NavLink>
@@ -72,7 +84,7 @@ const SuperAdminMainDashBoard = () => {
               <BsFillPeopleFill></BsFillPeopleFill> Artists
             </NavLink>
           </li>
-          
+
           <li>
             <NavLink className="p-3 text-base" to="managePrison">
               <FaBookmark></FaBookmark> Prisons / Organizations
@@ -88,7 +100,6 @@ const SuperAdminMainDashBoard = () => {
               <FaBookmark></FaBookmark> Settings
             </NavLink>
           </li>
-          
         </ul>
       </div>
     </div>
