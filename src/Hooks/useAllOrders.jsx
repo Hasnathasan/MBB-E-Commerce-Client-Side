a8dcee4de2f0e692b9dafca676a10529862a0653
Hasnathasan <hasnatoooooooo@gmail.com>
2024-03-12 19:45:34 +0600

    useAllOrders.jsx file formated

--- src/Hooks/useAllOrders.jsx
+++ src/Hooks/useAllOrders.jsx
@@ -3,21 +3,20 @@ import useAxiosSecure from "./useAxiosSecure";
 import { AuthContext } from "../Providers/AuthProvider";
 import { useQuery } from "@tanstack/react-query";
 
-
 const useAllOrders = () => {
-    const [axiosSecure] = useAxiosSecure();
-    const {
-      data: orders,
-      isLoading: isOrdersLoading,
-      refetch,
-    } = useQuery({
-      queryKey: ["allOrdersForAdmin"],
-      queryFn: async () => {
-        const res = await axiosSecure.get(`/allOrders`);
-        return res.data;
-      },
-    });
-    return [orders, isOrdersLoading, refetch];
+  const [axiosSecure] = useAxiosSecure();
+  const {
+    data: orders,
+    isLoading: isOrdersLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["allOrdersForAdmin"],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/allOrders`);
+      return res.data;
+    },
+  });
+  return [orders, isOrdersLoading, refetch];
 };
 
 export default useAllOrders;
