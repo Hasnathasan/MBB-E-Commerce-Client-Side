82ba8d1712f0afea59754c1778ddc941a95438dc
Hasnathasan <hasnatoooooooo@gmail.com>
2024-03-14 23:36:23 +0600

    useUserCart.jsx file formated

--- src/Hooks/useUserCart.jsx
+++ src/Hooks/useUserCart.jsx
@@ -2,18 +2,21 @@ import useAxiosSecure from "./useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
 import useUser from "./useUser";
 
-
 const useUserCart = () => {
-    const [axiosSecure] = useAxiosSecure()
-    const [userData] = useUser();
-    const { data: userCart, isLoading: isUserCartLoading, refetch } = useQuery({
-        queryKey: [`cartOf-${userData?.email}`, userData],
-        queryFn: async() => {
-            const res = await axiosSecure.get(`userCart/${userData?.email}`)
-            return res.data;
-        },
-      })
-      return [userCart, isUserCartLoading, refetch];
+  const [axiosSecure] = useAxiosSecure();
+  const [userData] = useUser();
+  const {
+    data: userCart,
+    isLoading: isUserCartLoading,
+    refetch,
+  } = useQuery({
+    queryKey: [`cartOf-${userData?.email}`, userData],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`userCart/${userData?.email}`);
+      return res.data;
+    },
+  });
+  return [userCart, isUserCartLoading, refetch];
 };
 
 export default useUserCart;
