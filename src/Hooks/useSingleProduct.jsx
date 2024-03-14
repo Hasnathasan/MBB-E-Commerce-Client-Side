c18a5782e9e37dcc43f19d7f87488f38243248f1
Hasnathasan <hasnatoooooooo@gmail.com>
2024-03-14 23:36:56 +0600

    useSingleProduct.jsx file formated

--- src/Hooks/useSingleProduct.jsx
+++ src/Hooks/useSingleProduct.jsx
@@ -1,21 +1,20 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "./useAxiosSecure";
 
-
-const useSingleProduct = ({id}) => {
-    const [axiosSecure] = useAxiosSecure();
-    const {
-      data: product,
-      isLoading: isProductLoading,
-      refetch,
-    } = useQuery({
-      queryKey: ["singleProduct", id],
-      queryFn: async () => {
-        const res = await axiosSecure.get(`/singleProduct/${id}`);
-        return res.data;
-      },
-    });
-    return [product, isProductLoading, refetch];
+const useSingleProduct = ({ id }) => {
+  const [axiosSecure] = useAxiosSecure();
+  const {
+    data: product,
+    isLoading: isProductLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["singleProduct", id],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/singleProduct/${id}`);
+      return res.data;
+    },
+  });
+  return [product, isProductLoading, refetch];
 };
 
 export default useSingleProduct;
--- src/Hooks/useUserOrders.jsx
+++ src/Hooks/useUserOrders.jsx
@@ -3,21 +3,22 @@ import useAxiosSecure from "./useAxiosSecure";
 import { useContext } from "react";
 import { AuthContext } from "../Providers/AuthProvider";
 
+
 const useUserOrders = () => {
-  const [axiosSecure] = useAxiosSecure();
-  const { user } = useContext(AuthContext);
-  const {
-    data: orders,
-    isLoading: isOrdersLoading,
-    refetch,
-  } = useQuery({
-    queryKey: ["orders", user],
-    queryFn: async () => {
-      const res = await axiosSecure.get(`/orders/${user?.email}`);
-      return res.data;
-    },
-  });
-  return [orders, isOrdersLoading, refetch];
+    const [axiosSecure] = useAxiosSecure();
+    const {user} = useContext(AuthContext);
+    const {
+      data: orders,
+      isLoading: isOrdersLoading,
+      refetch,
+    } = useQuery({
+      queryKey: ["orders", user],
+      queryFn: async () => {
+        const res = await axiosSecure.get(`/orders/${user?.email}`);
+        return res.data;
+      },
+    });
+    return [orders, isOrdersLoading, refetch];
 };
 
 export default useUserOrders;
