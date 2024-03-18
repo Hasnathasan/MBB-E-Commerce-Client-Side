7b6841d8935ba6f213c3893614722dd797884cb9
Hasnathasan <hasnatoooooooo@gmail.com>
2024-03-18 22:35:53 +0600

    Sales report page modified

--- src/Dashboard/SuperAdminDashBoard/SalesReport/SalesReport.jsx
+++ src/Dashboard/SuperAdminDashBoard/SalesReport/SalesReport.jsx
@@ -1,4 +1,4 @@
-import { Avatar, Select, SelectItem } from "@nextui-org/react";
+import { Avatar, Button, Select, SelectItem, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from "@nextui-org/react";
 import useArtists from "../../../Hooks/useArtists";
 import { useState } from "react";
 import Loader from "../../../Components/Loader/Loader";
@@ -52,6 +52,43 @@ const SalesReport = () => {
                 <div>
 h
                 </div>
+            </div>
+
+            <div>
+            <Table aria-label="Example table with custom cells">
+      <TableHeader>
+        <TableColumn>Id</TableColumn>
+        <TableColumn>Seller</TableColumn>
+        <TableColumn>Commision</TableColumn>
+        <TableColumn>Status</TableColumn>
+        <TableColumn>Action</TableColumn>
+      </TableHeader>
+      <TableBody>
+          <TableRow key={products?._id}>
+            <TableCell>
+              <div className="flex justify-start items-center gap-3">
+                <h3>{products?._id}</h3>
+              </div>
+            </TableCell>
+            <TableCell>{products?.artistEmail}</TableCell>
+            <TableCell>
+              <div className="flex justify-center gap-2 items-center">
+                <div>$577</div>
+                <span>|</span>
+                <div>$234</div>
+                <span>|</span>
+                <div>$456</div>
+              </div>
+            </TableCell>
+            <TableCell>{products?.rating}</TableCell>
+            <TableCell>
+              <Button color="success" radius="lg" className="text-white">
+                View Details
+              </Button>
+            </TableCell>
+          </TableRow>
+      </TableBody>
+    </Table>
             </div>
         </div>
     );
