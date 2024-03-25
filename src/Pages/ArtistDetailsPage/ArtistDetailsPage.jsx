26a4730ae813d63ab597a4e9946b2e1b57fd94d0
Hasnathasan <hasnatoooooooo@gmail.com>
2024-03-25 18:53:02 +0600

    Artist.jsx file formated

--- src/Pages/ArtistDetailsPage/ArtistDetailsPage.jsx
+++ src/Pages/ArtistDetailsPage/ArtistDetailsPage.jsx
@@ -15,15 +15,15 @@ const ArtistDetailsPage = () => {
   }
   console.log(artistData, products);
   return (
-    <div className=" mx-8 my-8">
-      <div className="grid grid-cols-12 justify-center gap-6 border border-gray-300 p-5 rounded-lg ">
-        <div className=" col-span-5 flex flex-col justify-center items-center gap-2">
+    <div className="mx-3 lg:mx-8 my-8">
+      <div className="grid grid-cols-12 justify-center gap-6 border border-gray-300 p-3 lg:p-5 rounded-lg ">
+        <div className=" col-span-12 lg:col-span-5 flex flex-col justify-center items-center gap-2">
           <Avatar src={artistData?.userPhoto} className="w-44 h-44 text-large" />
           <h2 className="text-2xl font-semibold">
             {artistData?.userName || "Unknown"}
           </h2>
         </div>
-        <div className="col-span-7 flex justify-center items-center">
+        <div className="lg:col-span-7 col-span-12 flex justify-center items-center">
         <ReactPlayer url={artistData?.bio_video_link} />
         </div>
       </div>
@@ -60,7 +60,7 @@ const ArtistDetailsPage = () => {
             </p>
 
             <h2 className="text-lg font-semibold mb-5 text-gray-900">Correctional System: <span className="text-gray-800">Unknown</span></h2>
-            <h2 className="text-lg font-semibold mb-5 space-x-3 text-gray-900">Keywords: {artistData?.keyWords?.map(keyword => <span key={keyword} className="text-gray-800">{keyword}</span>)}</h2>
+            <h2 className="text-lg font-semibold mb-5 space-x-3 text-gray-900">Keywords: {artistData?.keywords?.map(keyword => <span key={keyword} className="text-gray-800">{keyword},</span>)}</h2>
             <h2 className="text-lg font-semibold text-gray-900">State: <span className="text-gray-800">{artistData?.billingInfo?.states}</span></h2>
             
           </Tab>
@@ -74,7 +74,7 @@ const ArtistDetailsPage = () => {
             }
           >
              {
-              products?.length !== 0 ? <div className={`grid grid-cols-2 mt-5 px-3 md:px-14  gap-6 justify-center items-center  ${location.pathname.includes("/adminDashboar")? "md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4" : "sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5"}`}>
+              products?.length !== 0 ? <div className={`grid grid-cols-1 md:grid-cols-2 mt-5 px-1 lg:px-3 md:px-14  gap-6 justify-center items-center  ${location.pathname.includes("/adminDashboar")? "md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4" : "sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5"}`}>
               {products?.map((product) => (
                 <PopularProductsCard
                   key={product?.name}
--- src/Pages/Artists/Artists.jsx
+++ src/Pages/Artists/Artists.jsx
@@ -3,31 +3,38 @@ import useArtists from "../../Hooks/useArtists";
 import { Link } from "react-router-dom";
 import Loader from "../../Components/Loader/Loader";
 
-
 const Artists = () => {
-    const [artistsData, isArtistsDataLoading] = useArtists();
-    if(isArtistsDataLoading){
-        return <Loader></Loader>
-    }
-    console.log(artistsData);
-    return (
-        <div className="mx-8 py-9">
-            <div className="flex justify-between items-center mb-5">
-            <h1 className="text-2xl font-semibold">All Arists</h1>
-            <h1 className="text-sm text-gray-800  font-semibold">Total {artistsData?.length} Artist&apos;s found</h1>
+  const [artistsData, isArtistsDataLoading] = useArtists();
+  if (isArtistsDataLoading) {
+    return <Loader></Loader>;
+  }
+  console.log(artistsData);
+  return (
+    <div className="lg:mx-8 mx-2 py-9">
+      <div className="flex justify-between items-center mb-5">
+        <h1 className="text-2xl font-semibold">All Arists</h1>
+        <h1 className="text-sm text-gray-800  font-semibold">
+          Total {artistsData?.length} Artist&apos;s found
+        </h1>
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5">
+        {artistsData?.map((artist) => (
+          <Link to={`/artistDetails/${artist?.email}`} key={artist?._id}>
+            <div className="flex bg-white border-2 transition-all duration-300 items-center p-6 border-gray-300 rounded-lg hover:border-green-500 flex-col gap-2">
+              <Avatar
+                src={artist?.userPhoto}
+                className="lg:w-40 w-24 lg:h-40 h-24 text-large"
+              />
+              <h3 className="lg:text-xl font-semibold">{artist.userName}</h3>
+              <h4 className="text-sm font-medium">
+                Total Products: {artist?.total_products || 0}
+              </h4>
             </div>
-            <div className="grid grid-cols-5 gap-5">
-            {
-                artistsData?.map(artist => <Link to={`/artistDetails/${artist?.email}`} key={artist?._id}>
-                <div className="flex bg-white border-2 transition-all duration-300 items-center p-6 border-gray-300 rounded-lg hover:border-green-500 flex-col gap-2">
-                    <Avatar src={artist?.userPhoto} className="w-40 h-40 text-large" />
-                    <h3 className="text-xl font-semibold">{artist.userName}</h3>
-                    <h4 className="text-sm font-medium">Total Products: {artist?.total_products}</h4>
-                </div></Link>)
-            }
-            </div>
-        </div>
-    );
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Artists;
