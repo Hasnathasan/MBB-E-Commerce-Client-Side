af02afa2ecccef88917eb542a7d329fb24878911
Hasnathasan <hasnatoooooooo@gmail.com>
2024-03-29 17:42:42 +0600

    updated

--- src/Pages/ArtistDetailsPage/ArtistDetailsPage.jsx
+++ src/Pages/ArtistDetailsPage/ArtistDetailsPage.jsx
@@ -6,25 +6,28 @@ import ReactPlayer from "react-player";
 import useArtistProductsByEmail from "../../Hooks/useArtistProductsByEmail";
 import Loader from "../../Components/Loader/Loader";
 const ArtistDetailsPage = () => {
-  const {email} = useParams();
-  const [artistData, isArtistDataLoading] = useArtist({email});
-  const [products, isProductsLoading] = useArtistProductsByEmail({email});
+  const { email } = useParams();
+  const [artistData, isArtistDataLoading] = useArtist({ email });
+  const [products, isProductsLoading] = useArtistProductsByEmail({ email });
   const location = useLocation();
-  if(isArtistDataLoading || isProductsLoading){
-    return <Loader></Loader>
+  if (isArtistDataLoading || isProductsLoading) {
+    return <Loader></Loader>;
   }
   console.log(artistData, products);
   return (
     <div className="mx-3 lg:mx-8 my-8">
       <div className="grid grid-cols-12 justify-center gap-6 border border-gray-300 p-3 lg:p-5 rounded-lg ">
         <div className=" col-span-12 lg:col-span-5 flex flex-col justify-center items-center gap-2">
-          <Avatar src={artistData?.userPhoto} className="w-44 h-44 text-large" />
+          <Avatar
+            src={artistData?.userPhoto}
+            className="w-44 h-44 text-large"
+          />
           <h2 className="text-2xl font-semibold">
             {artistData?.userName || "Unknown"}
           </h2>
         </div>
         <div className="lg:col-span-7 col-span-12 flex justify-center items-center">
-        <ReactPlayer url={artistData?.bio_video_link} />
+          <ReactPlayer url={artistData?.bio_video_link} />
         </div>
       </div>
       <div className="mt-7">
@@ -51,18 +54,30 @@ const ArtistDetailsPage = () => {
             }
           >
             <h2 className="text-lg font-semibold text-gray-900">Bio:</h2>
-            <p className="mb-6">
-              {artistData?.bio}
-            </p>
-            <h2 className="text-lg font-semibold text-gray-900">Art Description:</h2>
-            <p className="mb-6">
-              {artistData?.art_description}
-            </p>
+            <p className="mb-6">{artistData?.bio}</p>
+            <h2 className="text-lg font-semibold text-gray-900">
+              Art Description:
+            </h2>
+            <p className="mb-6">{artistData?.art_description}</p>
 
-            <h2 className="text-lg font-semibold mb-5 text-gray-900">Correctional System: <span className="text-gray-800">Unknown</span></h2>
-            <h2 className="text-lg font-semibold mb-5 space-x-3 text-gray-900">Keywords: {artistData?.keywords?.map(keyword => <span key={keyword} className="text-gray-800">{keyword},</span>)}</h2>
-            <h2 className="text-lg font-semibold text-gray-900">State: <span className="text-gray-800">{artistData?.billingInfo?.states}</span></h2>
-            
+            <h2 className="text-lg font-semibold mb-5 text-gray-900">
+              Correctional System:{" "}
+              <span className="text-gray-800">Unknown</span>
+            </h2>
+            <h2 className="text-lg font-semibold mb-5 space-x-3 text-gray-900">
+              Keywords:{" "}
+              {artistData?.keywords?.map((keyword) => (
+                <span key={keyword} className="text-gray-800">
+                  {keyword},
+                </span>
+              ))}
+            </h2>
+            <h2 className="text-lg font-semibold text-gray-900">
+              State:{" "}
+              <span className="text-gray-800">
+                {artistData?.billingInfo?.states}
+              </span>
+            </h2>
           </Tab>
           <Tab
             key="music"
@@ -73,17 +88,30 @@ const ArtistDetailsPage = () => {
               </div>
             }
           >
-             {
-              products?.length !== 0 ? <div className={`grid grid-cols-1 md:grid-cols-2 mt-5 px-1 lg:px-3 md:px-14  gap-6 justify-center items-center  ${location.pathname.includes("/adminDashboar")? "md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4" : "sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5"}`}>
-              {products?.map((product) => (
-                <PopularProductsCard
-                  key={product?.name}
-                  product={product}
-                  isRounded={true}
-                ></PopularProductsCard>
-              ))}
-            </div> : <div className="my-16"> <h1 className="text-3xl text-center font-semibold">No Products Available</h1></div>
-             }
+            {products?.length !== 0 ? (
+              <div
+                className={`grid grid-cols-1 md:grid-cols-2 mt-5 px-1 lg:px-3 md:px-14  gap-6 justify-center items-center  ${
+                  location.pathname.includes("/adminDashboar")
+                    ? "md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4"
+                    : "sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5"
+                }`}
+              >
+                {products?.map((product) => (
+                  <PopularProductsCard
+                    key={product?.name}
+                    product={product}
+                    isRounded={true}
+                  ></PopularProductsCard>
+                ))}
+              </div>
+            ) : (
+              <div className="my-16">
+                {" "}
+                <h1 className="text-3xl text-center font-semibold">
+                  No Products Available
+                </h1>
+              </div>
+            )}
           </Tab>
         </Tabs>
       </div>
