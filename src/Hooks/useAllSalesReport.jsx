380f3c2eb00d114a7b15b1144f476ea8dd3bf669
Hasnathasan <hasnatoooooooo@gmail.com>
2024-03-30 14:42:42 +0600

    useAllSalesreport.jsx hook updated

--- src/Hooks/useAllSalesReport.jsx
+++ src/Hooks/useAllSalesReport.jsx
@@ -1,21 +1,22 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "./useAxiosSecure";
 
-
-const useAllSalesReport = ({status}) => {
-    const [axiosSecure] = useAxiosSecure();
-    const {
-      data: salesReport,
-      isLoading: isSalesReportLoading,
-      refetch,
-    } = useQuery({
-      queryKey: ["sales-report-all", status],
-      queryFn: async () => {
-        const res = await axiosSecure.get(`/sales-report-all/${status ? `?status=${status}` : ""}`);
-        return res.data;
-      },
-    });
-    return [salesReport, isSalesReportLoading, refetch];
+const useAllSalesReport = ({ status }) => {
+  const [axiosSecure] = useAxiosSecure();
+  const {
+    data: salesReport,
+    isLoading: isSalesReportLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["sales-report-all", status],
+    queryFn: async () => {
+      const res = await axiosSecure.get(
+        `/sales-report-all/${status ? `?status=${status}` : ""}`
+      );
+      return res.data;
+    },
+  });
+  return [salesReport, isSalesReportLoading, refetch];
 };
 
 export default useAllSalesReport;
