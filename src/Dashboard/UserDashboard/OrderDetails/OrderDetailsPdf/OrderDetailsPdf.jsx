bc6f48361481dcb523046686e75d8eefa8b66b1b
Hasnathasan <hasnatoooooooo@gmail.com>
2024-04-22 15:51:55 +0600

    OrderDetailsPDF updated

--- src/Dashboard/UserDashboard/OrderDetails/OrderDetailsPdf/OrderDetailsPdf.jsx
+++ src/Dashboard/UserDashboard/OrderDetails/OrderDetailsPdf/OrderDetailsPdf.jsx
@@ -96,7 +96,9 @@ const OrderDetailsPdf = ({ order }) => {
                   <Text style={styles.subtitle}>Billing Address</Text>
                   <Text style={styles.text}> {userName || "Unknown"}</Text>
                   <Text style={styles.text}>{address || "Unknown"}, </Text>
-                  <Text style={styles.text}>{states}, {zipCode} </Text>
+                  <Text style={styles.text}>
+                    {states}, {zipCode}{" "}
+                  </Text>
                   <Text style={styles.text}> {email || "Unknown"}</Text>
                   <Text style={styles.text}>
                     {userPhoneNumber || "Unknown"}
@@ -108,16 +110,17 @@ const OrderDetailsPdf = ({ order }) => {
                     {order?.shipping_address?.userName || userName || "Unknown"}
                   </Text>
                   <Text style={styles.text}>
-                    {order?.shipping_address?.address || address || "Unknown"}, 
+                    {order?.shipping_address?.address || address || "Unknown"},
                   </Text>
                   <Text style={styles.text}>
-                  {order?.shipping_address?.states || states}, {order?.shipping_address?.zipCode || zipCode}
+                    {order?.shipping_address?.states || states},{" "}
+                    {order?.shipping_address?.zipCode || zipCode}
                   </Text>
                   <Text style={styles.text}>
-                  {order?.shipping_address?.email || email || "Unknown"}
+                    {order?.shipping_address?.email || email || "Unknown"}
                   </Text>
                   <Text style={styles.text}>
-                  {order?.shipping_address?.userPhoneNumber ||
+                    {order?.shipping_address?.userPhoneNumber ||
                       userPhoneNumber ||
                       "Unknown"}
                   </Text>
