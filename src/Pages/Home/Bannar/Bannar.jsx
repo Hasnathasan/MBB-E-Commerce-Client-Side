09b8a94ac3e83d12a02461cd91bfe51f5a483e6b
Hasnathasan <hasnatoooooooo@gmail.com>
2024-04-25 23:47:23 +0600

    Updated

--- src/Pages/Home/Bannar/Bannar.jsx
+++ src/Pages/Home/Bannar/Bannar.jsx
@@ -6,63 +6,64 @@ import Loader from "../../../Components/Loader/Loader";
 import { Link } from "react-router-dom";
 
 const Bannar = () => {
-    const [bannerImages, isBannerImagesLoading, refetch] = useBannerImages();
-    if(isBannerImagesLoading){
-        return <Loader></Loader>
-    }
-    console.log(bannerImages);
-    const settings = {
-        fade: true,
-        infinite: true,
-        autoplay: true,
-        arrows: false,
-        speed: 700,
-        autoplaySpeed: 3000,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-            },
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              initialSlide: 1,
-            },
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              fade: false,
-              centerMode: true,
-              centerPadding: "20px",
-            },
-          },
-        ],
-      };
-    return (
-        <div className="w-full mt-4 mb-10 mx-auto">
-            <Slider {...settings} className="">
+  const [bannerImages, isBannerImagesLoading, refetch] = useBannerImages();
+  if (isBannerImagesLoading) {
+    return <Loader></Loader>;
+  }
+  console.log(bannerImages);
+  const settings = {
+    fade: true,
+    infinite: true,
+    autoplay: true,
+    arrows: false,
+    speed: 700,
+    autoplaySpeed: 3000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          fade: false,
+          centerMode: true,
+          centerPadding: "20px",
+        },
+      },
+    ],
+  };
+  return (
+    <div className="w-full mt-4 mb-10 mx-auto">
+      <Slider {...settings} className="">
         {bannerImages?.map((banner, index) => (
           <div className="w-full" key={index}>
             <Link target="_blank" className="w-full h-full" to={banner?.link}>
-            <img
-              className=" rounded-[10px] md:rounded-none p-[5px] md:p-0 h-[180px] md:h-[500px] w-full"
-              src={banner?.img}
-            /></Link>
+              <img
+                className=" rounded-[10px] md:rounded-none p-[5px] md:p-0 h-[180px] md:h-[500px] w-full"
+                src={banner?.img}
+              />
+            </Link>
           </div>
         ))}
       </Slider>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default Bannar;
